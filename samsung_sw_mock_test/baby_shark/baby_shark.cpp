26ec1c1bcf541e70cffa1c44f06ce42784dc8375
Moongee Cho <whansrl92@gmail.com>
2019-04-11 22:50:13 +0900

    Update baby_shark.cpp

--- samsung_sw_mock_test/baby_shark/baby_shark.cpp
+++ samsung_sw_mock_test/baby_shark/baby_shark.cpp
@@ -5,110 +5,127 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
+
 using namespace std;
 
 const int NMAX = 20;
 
-int moving[4][2] = {{0, -1}, {-1, 0}, {1, 0}, {0, 1}};
 int n;
 
-int arr[NMAX][NMAX];
-bool visited[NMAX][NMAX];
+struct shark {
+	int x;
+	int y;
+	int dist;
+	shark() {}
+	shark(int nx, int ny, int nd) {
+		x = nx;
+		y = ny;
+		dist = nd;
+	}
+};
 
-int sharkX;
-int sharkY;
-int sharkSize;
+int moving[4][2] = { {-1,0}, {0,-1},{0,1},{1,0} };
+int map[NMAX][NMAX];
+bool visit[NMAX][NMAX];
+int fcnt, ssize;
+shark start;
 
-int totalFish;
-
-int totalDistance;
-
-pair<int, pair<int,int>> bfs() {
-    queue<pair<int, pair<int,int>>> q;
-    vector<pair<int, pair<int,int>>> eatenFish;
-
-    for(int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            visited[i][j] = false;
-        }
-    }
-
-    q.push(make_pair(0, make_pair(sharkX, sharkY)));
-
-    while(!q.empty()) {
-        int prevX = q.front().second.first;
-        int prevY = q.front().second.second;
-        int prevDist = q.front().first;
-
-        q.pop();
-
-        for(int i = 0; i < 4; i++) {
-            int newX = prevX + moving[i][0];
-            int newY = prevY + moving[i][1];
-            if(newX >=0 && newX < n && newY >= 0 && newY < n) {
-                if(!visited[newX][newY] && arr[newX][newY] <= sharkSize) {
-                    int newDist = prevDist + 1;
-                    visited[newX][newY] = true;
-                    if (arr[newX][newY] < sharkSize && arr[newX][newY] != 0)
-                        eatenFish.push_back(make_pair(newDist, make_pair(newX, newY)));
-                    q.push(make_pair(newDist, make_pair(newX, newY)));
-                }
-            }
-        }
-    }
-
-    if(eatenFish.size() == 0)
-        return make_pair(-1, make_pair(-1, -1));
-    else {
-        sort(eatenFish.begin(), eatenFish.end());
-        return eatenFish[0];
-    }
+void init() {
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			visit[i][j] = false;
 }
 
+bool compare(shark one, shark two) {
+	if (one.dist == two.dist) {
+		if (one.x == two.x)
+			return one.y < two.y;
+		else
+			return one.x < two.x;
+	}
+	else
+		return one.dist < two.dist;
+}
 
-void solve() {
-    sharkSize = 2;
-    arr[sharkX][sharkY] = 0;
-    totalDistance = 0;
+shark bfs() {
+	queue<shark> q;
+	vector<shark> fish;
 
-    int eatCount = 0;
-    int totalEatCount = 0;
+	init();
 
-    while(totalEatCount < totalFish) {
-        pair<int, pair<int, int>> next = bfs();
-        if(next.first < 0)
-            break;
-        else {
-            sharkX = next.second.first;
-            sharkY = next.second.second;
-            totalDistance += next.first;
-            arr[sharkX][sharkY] = 0;
-            eatCount++;
-            totalEatCount++;
-        }
+	q.push(start);
+	visit[start.x][start.y] = true;
 
-        if(eatCount == sharkSize) {
-            sharkSize++;
-            eatCount = 0;
-        }
-    }
+
+	while (!q.empty()) {
+		shark cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = cur.x + moving[i][0];
+			int ny = cur.y + moving[i][1];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visit[nx][ny]) {
+				if (map[nx][ny] <= ssize) {
+					visit[nx][ny] = true;
+					if (map[nx][ny] < ssize && map[nx][ny] != 0) {
+						fish.push_back(shark(nx, ny, cur.dist + 1));
+					}
+					q.push(shark(nx, ny, cur.dist + 1));
+				}
+			}
+		}
+	}
+
+	if (!fish.size()) {
+		return shark(0, 0, 0);
+	}
+	else {
+		sort(fish.begin(), fish.end(), compare);
+		return fish[0];
+	}
+}
+
+int solve() {
+	ssize = 2;
+	int eat = 0;
+	int ecnt = 0;
+	int dist = 0;
+	while (ecnt < fcnt) {
+		shark next = bfs();
+		if (next.dist == 0)
+			break;
+		else {	// found
+			dist += next.dist;
+			eat++;
+			ecnt++;
+			start = shark(next.x, next.y, 0);
+			map[next.x][next.y] = 0;
+		}
+
+		if (eat == ssize) {
+			ssize++;
+			eat = 0;
+		}
+	}
+
+	return dist;
 }
 
 int main() {
-    cin >> n;
-    totalFish = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cin >> arr[i][j];
-            if(arr[i][j] == 9) {
-                sharkX = i;
-                sharkY = j;
-            } else if (arr[i][j] != 0)
-                totalFish++;
-        }
-    }
+	cin >> n;
 
-    solve();
+	fcnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] > 0 && map[i][j] <= 6)
+				fcnt++;
+			if (map[i][j] == 9) {
+				start = shark(i, j, 0);
+				map[i][j] = 0;
+			}
+		}
+	}
 
-    cout << totalDistance << endl;
+	cout << solve() << endl;
 }
