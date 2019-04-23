5887f33673ff71ac75f8a296e81203c72155f353
Moongee Cho <whansrl92@gmail.com>
2019-04-24 00:47:11 +0900

    Create sorting_cordinates.cpp

---
+++ sorting/sorting_cordinates/sorting_cordinates.cpp
@@ -0,0 +1,33 @@
+/*
+ * https://www.acmicpc.net/problem/11650
+ */
+
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int,int> &first, pair<int,int> &second) {
+    if(first.first == second.first) {
+        return first.second < second.second;
+    } else
+        return first.first < second.first;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    vector<pair<int,int>> arr(n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", &arr[i].first, &arr[i].second);
+    }
+
+    sort(arr.begin(), arr.end(), compare);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d %d\n", arr[i].first, arr[i].second);
+    }
+}
